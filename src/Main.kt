e6df8afa65383ec97a103c283a24fa2e1111d5e3
viictorcarvalho <you@example.com>
2022-05-30 17:43:25 -0300

    Backup do Main

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,14 @@
 fun main() {
 
+    print("Por favor, insira seu nome: ")
+    val nomeCliente = readln()
+    println(ESPACAMENTO)
+
+    println(HIFEN)
+    print("Seja bem-vindo(a) ao melhor FastFood de SimCity, $nomeCliente! \n")
+    println(HIFEN)
+
+    val menu = Menu()
+    menu.menuPrimario()
+
 }
